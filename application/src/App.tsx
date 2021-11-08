5c6fce563eeb2156fb85d8e35c219962ef40f551
Pyotr Petkov <pyotr.petkov@gmail.com>
2021-11-08 22:55:08 +0200

    connect server to app

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:3005/graphql/src',
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div className="App"></div>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express = require('express')
 import graphQL = require('express-graphql')
 import schema = require('../schema/schema')
+import cors = require('cors')
 
 const { graphqlHTTP } = graphQL
 const { Schema } = schema
@@ -9,6 +10,8 @@ const app = express()
 const PORT = 3005
 
 app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }))
+
+app.use(cors())
 
 app.listen(PORT, () => {
   console.log('Server started!')
